c11b6eeeb9ec3faa59bc1e4412c80df6ca4fa0f5
keerthuuu <keerthana230406@gmail.com>
2026-09-03 11:51:56 +0530

    Delete frontend folder from repository

--- frontend/src/pages/ReportsPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReportCharts from '../components/reports/ReportCharts';
-
-export const ReportsPage = () => {
-  return <ReportCharts />;
-};
-
-export default ReportsPage;
